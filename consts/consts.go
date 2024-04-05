b87f5a9926a2c9563d9e2b57a5a0542379325f63
Josh <adesanyajoshua@ymail.com>
2024-04-05 09:48:43 +0100

    task

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "JoshToken"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "JoshTokenVM"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JT"
 )
 
 var ID ids.ID
